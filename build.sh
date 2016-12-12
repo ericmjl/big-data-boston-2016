c6085f8f683a00b9dd179ae7f6d7512d35aabc58
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-11 23:21:17 -0500

    added panama and influenza figures to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 convert -density 300 -resize 50% figures/networks.pdf figures/networks.png
 convert -density 300 -resize 50% figures/recommender.pdf figures/recommender.png
 convert -density 300 -resize 50% figures/collaborative-filtering.pdf figures/collaborative-filtering.png
+convert -density 300 -resize 50% figures/panama.pdf figures/panama.png
+convert -density 300 -resize 50% figures/influenza.pdf figures/influenza.png
 
 pandoc slides.md -t beamer -o slides.pdf
 pandoc slides.md -o index.html -H docs/css/styles.css
