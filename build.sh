702387378fa846a9a4560b19af7c9f1708e8cc6e
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-14 11:00:37 -0500

    updated build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # pandoc slides.md -o index.html -H docs/css/styles.css \
 #     --template=default.html
 #
-pandoc slides.md -t revealjs -o slides.html \
+pandoc slides.md --section-divs -t revealjs -o slides.html \
     --template=default.revealjs \
     -c styles.css -s
 
