41e8887df71c3ae6bbe1effcd19f92b483b81c9c
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-14 12:54:25 -0500

    renamed templates and removed beamer template

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ python build.py
 
 # Make HTML notes version of slides
 pandoc slides.md -o index.html -c css/notes.css \
-    --template=default.html -H header.html
+    --template=template.html -H header.html
 
 # Make revealjs version of slides
 pandoc --section-divs -t revealjs -s --template template.revealjs -o slides.html slides.md
