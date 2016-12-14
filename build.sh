cd7860456667630372154f154ad6d9b4694e23c3
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-14 13:19:18 -0500

    updated build script to use headers for slides

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ pandoc slides.md -o index.html -c css/notes.css \
     --template=template.html -H header.html
 
 # Make revealjs version of slides
-pandoc --section-divs -t revealjs -s --template template.revealjs -o slides.html slides.md
+pandoc --section-divs -t revealjs -s \
+    --template template.revealjs \
+    -o slides.html \
+    -H header.html \
+    slides.md
 
 # Automatically add and commit
 git add .
