65f4809082cdff3d6ed8fb69d91863ff46730c0a
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-14 11:35:33 -0500

    made revealjs version of slides!

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
-# python build.py
+python build.py
 
-# pandoc slides.md -t beamer -o slides.pdf \
-#     --template=default.beamer
-#
-# pandoc slides.md -o index.html -H docs/css/styles.css \
-#     --template=default.html
-#
-pandoc slides.md \
-    --section-divs \
-    -t revealjs \
-    --template=default.revealjs \
-    -c styles.css \
-    -s \
-    -o slides.html
+# Make PDF version of slides
+pandoc slides.md -t beamer -o slides.pdf \
+    --template=default.beamer
 
+# Make HTML notes version of slides
+pandoc slides.md -o index.html -H docs/css/styles.css \
+    --template=default.html
+
+# Make revealjs version of slides
+pandoc --section-divs -t revealjs -s --template template.revealjs -o slides.html slides.md
+
+# Automatically add and commit
 git add .
 git commit
 git push
